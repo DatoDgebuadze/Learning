c00c8f35180d8d1167c802167d9df39229fa5df0
DatoDgebuadze <davit.dgebuadze.1@btu.edu.ge>
2022-01-17 00:17:52 +0400

    Pushing existing code to github

--- canvas.js
+++ canvas.js
@@ -1,14 +1,18 @@
-var canvas = document.querySelector('canvas');
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-var c = canvas.getContext('2d');
-
-c.fillRect(100, 100, 100, 100); // x,y,width, height
-
-c.fillRect(300, 600, 100, 100);
+const s = window.screen;
+const w = matrix.width = s.width;
+const h = matrix.height = s.height;
+const ctx = matrix.getcontext('2d');
+const p = Array(Math.floor(w/10) +1).fill(0);
+const random = items => items[Math.floor(math.random() * items.length)];
+const text = 'აბგდევზთიკლმნოპრჯსტუფქღშჩწძქრ'.split("");
 
 
-console.log(canvas);
-
+setInterval(() => {
+    ctx.fillStyle='rgba(0,0,0,0.05)';
+    ctx.fillRect(0,0,w,h);
+    ctx.fillStyle = "#0f0";
+    p.map((v,i) => {
+        ctx.fillText(random(text), i * 10, v);
+        p[i] = v >= h || v > 50 + 10000 * Math.random() ? 0 : v + 10;
+    });
+}, 1000/30);
